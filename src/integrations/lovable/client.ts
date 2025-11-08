05301d6aa96890c918c1d973dd253ce5e9ac8326
sujangcolud <sujan.gcloud@gmail.com>
2025-11-08 22:14:58 +0545

    Add Lovable SDK setup

---
+++ src/integrations/lovable/client.ts
@@ -0,0 +1,9 @@
+// Lovable SDK setup
+import { LovableClient } from 'lovable-sdk';
+
+const client = new LovableClient({
+    project: 'gitcopilot',
+    secret: 'precioussn',
+});
+
+export default client;
